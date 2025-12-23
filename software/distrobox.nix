193e93a880f52460a8b4b537307985b5611b1b0a
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-23 19:43:08 +0800

    feat(distrobox): expose my profile to the container

--- software/distrobox.nix
+++ software/distrobox.nix
@@ -7,4 +7,8 @@
   };
 
   environment.systemPackages = [ pkgs.distrobox ];
+
+  environment.etc."distrobox/distrobox.conf".text = ''
+    container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+  '';
 }
