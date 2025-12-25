41cc5d42eaf65ec6ff11d52144334624727ffe1b
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-25 21:53:59 +0800

    feat: replace podman-compose with docker-compose

--- software/podman.nix
+++ software/podman.nix
@@ -17,6 +17,6 @@
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
-    podman-compose # start group of containers for dev
+    docker-compose # start group of containers for dev
   ];
 }
