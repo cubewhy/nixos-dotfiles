499c9b61b1b0a0783ff739c97abc84c877bf8467
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-27 10:09:12 +0800

    chore: deprecate the nodejs configuration

--- software/dev/nodejs.nix
+++ software/dev/nodejs.nix
@@ -1,3 +1,7 @@
+# Deprecared
+# Use
+# nix flake init --template "https://flakehub.com/f/the-nix-way/dev-templates/*#node"
+# instead
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     nodejs
