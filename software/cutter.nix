988e97a572e1405bde4ab7835e3dbf7a69ef50bb
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-12-30 18:57:07 +0800

    feat: rizin

--- software/cutter.nix
+++ software/cutter.nix
@@ -2,7 +2,6 @@
   environment.systemPackages = with pkgs; [
     (cutter.withPlugins (ps: with ps; [jsdec rz-ghidra sigdb]))
 
-    # Uncomment this line if you want to use rizin
-    # (rizin.withPlugins (ps: with ps; [jsdec rz-ghidra sigdb]))
+    (rizin.withPlugins (ps: with ps; [jsdec rz-ghidra sigdb]))
   ];
 }
