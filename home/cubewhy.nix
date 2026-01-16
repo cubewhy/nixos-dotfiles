dd0a7011a2799ff5de07d8bc45f057ea121c6d37
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-16 18:01:58 +0800

    feat: hidpi configuration using xsettingsd

--- home/cubewhy.nix
+++ home/cubewhy.nix
@@ -126,14 +126,26 @@
     EDITOR = "nvim";
   };
 
-  # home.pointerCursor = {
-  #   gtk.enable = true;
-  #   x11.enable = true;
-  #   package = pkgs.kdePackages.breeze;
-  #   name = "breeze_cursors";
-  #
-  #   size = 48;
-  # };
+  services.xsettingsd = {
+    enable = true;
+
+    settings = {
+      "Xft/DPI" = 196608;
+
+      "Xft/Antialias" = 1;
+      "Xft/Hinting" = 1;
+      "Xft/RGBA" = "rgb";
+      "Xft/HintStyle" = "hintslight";
+
+      "Net/ThemeName" = "Breeze";
+      "Net/IconThemeName" = "breeze";
+      "Gtk/CursorThemeName" = "breeze_cursors";
+
+      "Gdk/WindowScalingFactor" = 2;
+
+      "Gdk/UnscaledDPI" = 0;
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
