fea11f0336eda41ae31dc3d9daccef3f0ec69452
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-22 18:06:37 +0800

    chore: update jetbrains idea

--- overlays/jetbrains.nix
+++ overlays/jetbrains.nix
@@ -5,10 +5,10 @@ final: prev: {
     // {
       # --- IntelliJ IDEA Ultimate ---
       idea = prev.jetbrains.idea.overrideAttrs (old: rec {
-        version = "2025.3.1";
+        version = "2025.3.1.1";
         src = final.fetchurl {
           url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-          sha256 = "sha256-IeG5C17GhHZ6A0mLpCTvP5rMXb91nM7v1e3UdPBDrWw=";
+          sha256 = "sha256-OgZLIpYfPzm4ZrZLYoVY4ND3CNQjo/lWXUPw6BGWmXs=";
         };
       });
     };
