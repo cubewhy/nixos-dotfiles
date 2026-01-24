aeedd63a372a05e9a20291a99cdbf2bba8c82e78
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-24 19:50:29 +0800

    chore: remove the amd linux-firmware overlay

--- hardware/amdgpu.nix
+++ hardware/amdgpu.nix
@@ -1,15 +1,16 @@
 {...}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      linux-firmware = prev.linux-firmware.overrideAttrs (oldAttrs: {
-        postInstall =
-          (oldAttrs.postInstall or "")
-          + ''
-            cp ${./amdgpu-firmware}/* $out/lib/firmware/amdgpu/
-          '';
-      });
-    })
-  ];
+  # no need anymore since the new linux-firmware was published
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     linux-firmware = prev.linux-firmware.overrideAttrs (oldAttrs: {
+  #       postInstall =
+  #         (oldAttrs.postInstall or "")
+  #         + ''
+  #           cp ${./amdgpu-firmware}/* $out/lib/firmware/amdgpu/
+  #         '';
+  #     });
+  #   })
+  # ];
 
   services.xserver.videoDrivers = [
     "amdgpu"
