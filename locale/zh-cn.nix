5001084b674fd0c205cd420d510e4f6511ad70a9
cubewhy <61075476+cubewhy@users.noreply.github.com>
2026-01-27 21:42:38 +0800

    fix: zh-cn fonts

--- locale/zh-cn.nix
+++ locale/zh-cn.nix
@@ -16,7 +16,32 @@
   };
 
   fonts.packages = with pkgs; [
-    noto-fonts
     source-han-sans
+    source-han-serif
+    source-han-mono
+    noto-fonts
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = ["Source Han Serif SC" "Source Han Serif TC" "Noto Serif CJK SC" "DejaVu Serif"];
+      sansSerif = ["Source Han Sans SC" "Source Han Sans TC" "Noto Sans CJK SC" "DejaVu Sans"];
+      monospace = ["Source Han Mono SC" "Source Han Mono TC" "Noto Sans Mono CJK SC" "DejaVu Sans Mono"];
+    };
+  };
+
+  fonts.fontconfig.localConf = ''
+    <match target="pattern">
+      <test name="lang">
+        <string>zh-cn</string>
+      </test>
+      <test name="family">
+        <string>sans-serif</string>
+      </test>
+      <edit name="family" mode="prepend" binding="strong">
+        <string>Source Han Sans SC</string>
+      </edit>
+    </match>
+  '';
 }
