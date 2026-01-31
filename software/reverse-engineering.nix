888ec548b2c62b1945b517ffea3735c6afee1f3c
cubewhy <qby140326@gmail.com>
2026-01-31 19:06:17 +0800

    chore(re): remove rizin packages

--- software/reverse-engineering.nix
+++ software/reverse-engineering.nix
@@ -1,9 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    # (cutter.withPlugins (ps: with ps; [jsdec rz-ghidra sigdb]))
-
-    (rizin.withPlugins (ps: with ps; [jsdec rz-ghidra sigdb]))
-
     radare2
     # ghidra
     ltrace
