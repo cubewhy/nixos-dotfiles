fdd0a25632b483a76f3448a78bf9485af9e8314a
cubewhy <qby140326@gmail.com>
2026-02-03 15:12:27 +0800

    chore: update jetbrains idea

--- overlays/jetbrains.nix
+++ overlays/jetbrains.nix
@@ -5,10 +5,10 @@ final: prev: {
     // {
       # --- IntelliJ IDEA Ultimate ---
       idea = prev.jetbrains.idea.overrideAttrs (old: rec {
-        version = "2025.3.1.1";
+        version = "2025.3.2";
         src = final.fetchurl {
           url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-          sha256 = "sha256-OgZLIpYfPzm4ZrZLYoVY4ND3CNQjo/lWXUPw6BGWmXs=";
+          sha256 = "sha256-o0QsnxlTxm3LCCXpt4jH5077WG7b8dMO+LDfVT/hNuQ=";
         };
       });
     };
