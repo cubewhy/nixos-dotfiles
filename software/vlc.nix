4e6ec331dc856c162a43b86b0344164cba2b790d
cubewhy <qby140326@gmail.com>
2026-02-08 10:26:44 +0800

    fix: don't wrap vlc

--- software/vlc.nix
+++ software/vlc.nix
@@ -1,17 +1,18 @@
 {pkgs, ...}: {
-  environment.systemPackages = [
-    (pkgs.symlinkJoin {
-      name = "vlc";
-      paths = [pkgs.vlc];
-      nativeBuildInputs = [pkgs.makeWrapper];
-      postBuild = ''
-        wrapProgram $out/bin/vlc \
-          --unset DISPLAY
-
-        rm $out/share/applications/vlc.desktop
-        substitute "${pkgs.vlc}/share/applications/vlc.desktop" "$out/share/applications/vlc.desktop" \
-          --replace "Exec=${pkgs.vlc}/bin/vlc" "Exec=$out/bin/vlc"
-      '';
-    })
+  environment.systemPackages = with pkgs; [
+    vlc
+    # (pkgs.symlinkJoin {
+    #   name = "vlc";
+    #   paths = [pkgs.vlc];
+    #   nativeBuildInputs = [pkgs.makeWrapper];
+    #   postBuild = ''
+    #     wrapProgram $out/bin/vlc \
+    #       --unset DISPLAY
+    #
+    #     rm $out/share/applications/vlc.desktop
+    #     substitute "${pkgs.vlc}/share/applications/vlc.desktop" "$out/share/applications/vlc.desktop" \
+    #       --replace "Exec=${pkgs.vlc}/bin/vlc" "Exec=$out/bin/vlc"
+    #   '';
+    # })
   ];
 }
