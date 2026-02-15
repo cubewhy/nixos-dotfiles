7818848d79dcbf7ae9d2b5b15c1d23266c865780
cubewhy <qby140326@gmail.com>
2026-02-15 09:49:13 +0800

    Revert "feat: manage Ghidra via nix"
    This reverts commit 86ab6544ec2ce48169c3039abb41823157f734a7.

--- software/reverse-engineering.nix
+++ software/reverse-engineering.nix
@@ -1,33 +1,12 @@
-{
-  pkgs,
-  pkgs-unstable,
-  ...
-}: let
-  ghidraScripts = pkgs.stdenv.mkDerivation {
-    name = "ghidra-scripts-pack";
-    src = ./ghidra/scripts;
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out/
-    '';
-  };
-in {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     radare2
+    # ghidra
     ltrace
     bintools
     checksec
     strace
     hexdump
     xxd
-
-    (pkgs-unstable.ghidra.withExtensions (p:
-      with p; [
-        ret-sync
-        findcrypt
-        ghidra-firmware-utils
-        kaiju
-        ghidraScripts
-      ]))
   ];
 }
