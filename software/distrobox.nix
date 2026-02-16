3937b523b4c3ecadef9c069241d08aa13120e27e
cubewhy <qby140326@gmail.com>
2026-02-16 11:23:32 +0800

    feat: add nixos bin to distrobox

--- software/distrobox.nix
+++ software/distrobox.nix
@@ -11,5 +11,6 @@
 
   environment.etc."distrobox/distrobox.conf".text = ''
     container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+    PATH="$PATH:/run/host/run/current-system/sw/bin"
   '';
 }
