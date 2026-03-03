763ef189db7c6dd523b5d67cd486378276ab4f6b
cubewhy <qby140326@gmail.com>
2026-03-03 22:29:32 +0800

    feat: use docker as distrobox backend

--- software/distrobox.nix
+++ software/distrobox.nix
@@ -3,11 +3,11 @@
     (import ../overlays/distrobox.nix)
   ];
 
-  virtualisation.podman = {
-    enable = true;
-  };
-
   environment.systemPackages = [pkgs.distrobox];
+
+  environment.sessionVariables = {
+    DBX_CONTAINER_MANAGER = "docker";
+  };
 
   environment.etc."distrobox/distrobox.conf".text = ''
     container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
